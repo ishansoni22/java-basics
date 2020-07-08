a2901b7743e70ae4d7e4c240601be1a6b4e97e25
Ishan Soni <soni.ishan.nitj@gmail.com>
2020-07-08 20:02:08 +0530

    master
    Base64 encoding example

---
+++ src/main/java/com/ishan/javabasics/Main.java
@@ -0,0 +1,12 @@
+package com.ishan.javabasics;
+
+import com.ishan.javabasics.uuids.UniqueIDGenerator;
+
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println("Java Basics!");
+    System.out.println(UniqueIDGenerator.generate());
+  }
+
+}
---
+++ src/main/java/com/ishan/javabasics/uuids/UniqueIDGenerator.java
@@ -0,0 +1,29 @@
+package com.ishan.javabasics.uuids;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+public class UniqueIDGenerator {
+
+  private static final SecureRandom random = new SecureRandom();
+  private static final Base64.Encoder encoder = Base64.getUrlEncoder().withoutPadding();
+
+  public static String generate() {
+    String key = "ABC"; //All ASCII characters -> 3 bytes (Base64 will convert it into 4 bytes)
+    //vs
+    //String key = "•AB"; //The first is a non-ASCII characters and needs 3 bytes,
+    //Therefore produced Base64 output -> (5 * 8)/6 characters = 7
+
+    /**
+     Get the specific UUID (eg a 3 byte UUID(3 character - if ASCII/Unicode encoded)
+     will give a 4 byte or 4 character Base64 Key)
+     i.e 64*64*64*64 different combinations
+
+     Remember the system design interview questions?
+
+     **/
+    byte[] arr = key.getBytes();
+    return encoder.encodeToString(arr);
+  }
+
+}
